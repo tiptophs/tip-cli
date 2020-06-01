242e0826b36f2218f10d73843bf464b7359416cd
tiptophs <1085556180@qq.com>
2020-06-01 21:25:57 +0800

    feat: 完成简单的node命令行脚手架

--- tools/index.js
+++ tools/index.js
@@ -4,6 +4,18 @@
 const program = require("commander")
 //git地址下载工具
 const download = require("download-git-repo")
+//命令行交互工具
+const inquirer = require("inquirer")
+//模板引擎
+const handlebars = require("handlebars")
+//引入node文件读取
+const fs = require("fs")
+//引入视觉美化
+const ora = require("ora")
+//引入chalk增加文本样式
+const chalk = require("chalk")
+//引入文本前置图标
+const logSymbols = require("log-symbols")
 
 
 //使用Node开发命令行工具所执行javascript脚本，顶部必须引入#!/usr/bin/env node 生命
@@ -35,11 +47,45 @@ program
   .command('init <templateName> <projectName>')   //命令
   .description('初始化项目模板...')   //描述信息
   .action(function(templateName, projectName){
+    //下载文件之前，显示下载loading
+    const spinner = ora('正在下载模板...').start()
     //下载相应的文件
     const { downloadUrl } = templates[templateName]
-    console.log(downloadUrl, 'x')
     download(downloadUrl, projectName, {clone: true}, (err)=>{
-        console.log(err? '很遗憾，模板下载失败，请稍后重试。':'恭喜您，模板下载成功。')
+      //文件下载失败, 报错提示  
+      if(err){
+        spinner.fail()
+        console.log(logSymbols.error, chalk.red('很遗憾，模板下载失败，请稍后重试')) 
+        return
+      }
+      spinner.succeed()
+      //把要下载的模板的packjson文件读取出来
+      //使用向导的方式读取用户的配置
+      //使用模板引擎将用户输入的数据解析到packjson
+      //解析完毕后, 将新的文件重新写入到packjson文件中
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: '请输入项目名称'
+        },
+        {
+          type: 'input',
+          name: 'author',
+          message: '请输入创建用户名称'
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: '请输入项目描述信息'
+        }
+      ]).then((answers)=>{
+        const path = `${projectName}/package.json`
+        const packageContent = fs.readFileSync(path, 'utf-8')
+        const packageResult = handlebars.compile(packageContent)(answers)
+        fs.writeFileSync(path, packageResult)
+        console.log(logSymbols.success, chalk.green('恭喜您，模板初始化成功'))
+      })
     })
   });
 
